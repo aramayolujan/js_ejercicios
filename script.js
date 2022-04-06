6458f6be8d60b1bee4f212571bae277e702ab693
Maria Aramayo <marialujanaramayo@gmail.com>
2022-04-06 11:34:31 -0300

    ejercicio Crear un algoritmo utilizando un ciclo

--- script.js
+++ script.js
@@ -1,38 +1,9 @@
-/*let nombre = prompt("Cual es tu nombre");
-let apellido = prompt("Cual es tu apellido");
-let resultado = nombre + " " + apellido;
-console.log(resultado);*/
 
-/*let numero = prompt("edad")
-let resultado = parseInt(numero) + 2;
-console.log(resultado);*/
+let numero = parseInt(prompt('Ingrese un numero'));
 
-/*const validarEdad = (edad) => {
-    if (edad>=18){
-        alert('poder entrar');
-    }
-    else {
-        alert ('no podes entrar');
-    }
-    return
+for (let i =1; i<=5; i++) {
+
+    let resultado = i + numero ;
+    alert(numero + "+" + i + "=" + resultado);
 }
-let edadPersona = parseInt(prompt('Ingrese su edad'));
-validarEdad(edadPersona);*/
-
-for (i=1;i<10;i++){
-
-    document.write("<br><b>La tabla del " + i + ":</b><br>")
-    
-    for (j=1;j<10;j++) {
-    
-    document.write(i + " x " + j + ": ")
-    
-    document.write(i*j)
-    
-    document.write("<br>")
-    
-    }
-    
-    }
-    
 
