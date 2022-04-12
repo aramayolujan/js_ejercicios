c439e2d4d29284d40e654d27bfea1c6928e3ce74
Maria Aramayo <marialujanaramayo@gmail.com>
2022-04-12 16:04:30 -0300

    calcular precio final

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
+//Calcular precio final de un valor ingresado , agregandole IVA y aplicando un descuento//
+
+
+const suma  = (a,b) => a + b
+const multiplicar = (a,b) => a * b
+const iva   = x => x * 0.21
+
+
+let precioProducto =  parseInt(prompt('ingresar precio producto'));
+
+//Descuento del 30% sobre el precio del producto//
+let descuento = 0.70 
+
+let precioFinal= multiplicar(suma(precioProducto, iva(precioProducto)), descuento) 
+
+console.log(precioFinal)
+
 
 
