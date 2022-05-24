ac5bd0ce1486084db6810392858faacb66a376b8
Maria Aramayo <marialujanaramayo@gmail.com>
2022-05-24 11:04:40 -0300

    agregue .json

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+fetch("/js/productos.json")
+    .then( (response) => {
+        return response.json();
+    }).then( (productos) => {
+    console.log(productos);
+});
+
+
+/*
+
 //Calcular precio final de un valor ingresado , agregandole IVA y un porcentaje de ganancias//
 const suma  = (a,b) => a + b
 const multiplicar = (a,b) => a * b
@@ -16,24 +26,6 @@ btnPrecioResultado.onclick = () => {
     resultadoMuestra.innerHTML = `<p>  El precio final del producto es de: ${precioFinal}</p>`;
 };
 
-//incorporar array
-
-class producto {
-    constructor(nombre, categoria, precio) {
-        this.nombre = nombre;
-        this.categoria = categoria;
-        this.precio = precio;
-    }
-}
-
-const producto1 = new producto("Blazer Cler", "blazer", "$15.990.-")
-const producto2 = new producto("Blazer Paul", "blazer", "$8.752.-")
-const producto3 = new producto("Tapado Berlín", "tapado", "$13.500.-")
-const producto4 = new producto("Tapado Belix", "tapado", "$17.900.-")
-const producto5 = new producto("Camisa Boris", "camisa", "$11.700.-")
-const producto6 = new producto("Camisa Queen", "camisa", "$7.390.-")
-
-let productos = [ producto1 , producto2 , producto3 , producto4 , producto5 , producto6]
 
 //Agregar producto 
 productos.push(new producto("Jean Amanda","jean", "$9.690.-"))
@@ -96,4 +88,4 @@ function agregarLS (producto) {
 
         contenedor.append(ul);
     });
-    
+    */
